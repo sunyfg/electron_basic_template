2d9ee108642f09149cb78ab54eb1b499e8d7f587
sunyfg <yanfeng_sun@yeah.net>
2024-09-22 18:58:01 +0800

    test

--- renderer.js
+++ renderer.js
@@ -22,13 +22,4 @@ window.onmessage = event => {
 };
 
 // 测试
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
-
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type]);
-  }
-});
+window.postMessage('main-world-port', '*');
